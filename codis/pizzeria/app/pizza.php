2519d3a2549f8ab04ccb4711b9da8226a1296a1e
PussyDestroyer69 <xavigarcianieto@gmail.com>
2025-10-27 19:08:56 +0100

    Versió 1.2 del projecte pizzeria

--- codis/pizzeria/app/pizza.php
+++ codis/pizzeria/app/pizza.php
@@ -1,11 +1,11 @@
 <?php
 	$pvp = 10;
+	$IVA = 21;
 	if (empty($_GET['ingr'])) {
-		echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-		exit(0);
+		$pvp_public=$pvp*((100+$IVA)/100);
+		echo "El preu de la pizza bàsica és $pvp_public € (IVA inclòs)<br>";		
 	}
 	else {
-		$IVA=21;
 		$pvp_public=((100+$IVA)/100)*($pvp+count($_GET['ingr']));
 		echo "Has demanat la pizza bàsica amb:<br> ";
 		foreach ($_GET['ingr'] as $ingredient) {
